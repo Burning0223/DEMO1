464a579fb43ff03bca8de7b357979356b31153e7
Burning0223 <liuboning2004@126.com>
2026-05-06 22:55:31 +0800

    重构测试部分

--- run_trainer.py
+++ run_trainer.py
@@ -72,7 +72,7 @@ class Trainer():
         dev_acc=dev_metrics.acc
         return ave_loss,dev_acc
 
-    def save_checkpoint(self,train_dataloader,dev_dataloader):
+    def save_checkpoint(self,train_dataloader,dev_dataloader,test_dataloader):
         dev_best_acc=0.0
         for epoch in range(self.config.num_epochs):
             print(f"Epoch {epoch+1}")
@@ -88,11 +88,14 @@ class Trainer():
             checkpoint={
                 'epoch':epoch,
                 'model':self.model.state_dict(),
-                'optimizer':self.optimizer.state_dict(),
-                'scheduler':self.scheduler.state_dict(),
                 'loss':dev_loss,
                 'acc':dev_acc
             }
+            if self.optimizer is not None:
+                checkpoint["optimizer"] = self.optimizer.state_dict()
+
+            if self.scheduler is not None:
+                checkpoint["scheduler"] = self.scheduler.state_dict()
             torch.save(checkpoint,checkpoint_path)
             print(f"保存epoch{epoch+1}的checkpoint:{checkpoint_path}")
             if dev_acc>=dev_best_acc:
@@ -100,6 +103,7 @@ class Trainer():
                 best_model_path=checkpoint_path
                 print(f"当前模型最优准确率为：{dev_best_acc}")
                 print(f"保存最佳模型：{best_model_path}")
+            
             swanlab.log({
                 "epoch":epoch,
                 "训练损失":train_loss,
@@ -107,8 +111,39 @@ class Trainer():
                 "验证损失":dev_loss,
                 "验证准确率":dev_acc
             })
-            
+          
         print(f"模型最优准确率为：{dev_best_acc}")
+        test_loss,test_acc=self.test(test_dataloader,best_model_path) 
+        print(f"训练损失:{test_loss:.4f}")
+        print(f"训练准确率:{test_acc:.4f}")
+        swanlab.log({
+                "测试损失":test_loss,
+                "测试准确率":test_acc
+            })
+
+    def test(self,dataloader,best_model_path):
+        if not os.path.exists(best_model_path):
+            print("未找到最优模型，无法进行测试！")
+            return
+        else:
+            checkpoint=torch.load(best_model_path)
+            self.model.load_state_dict(checkpoint['model'])
+        self.model.eval()
+        total_loss=0.0
+        all_labels=[]
+        all_preds=[]
+        with torch.no_grad():
+            for batch in dataloader:
+                output,labels=self.model(**batch)
+                loss=self.loss_fn(output, labels)
+                preds=torch.argmax(output,dim=1)
+                all_labels.extend(labels.numpy())
+                all_preds.extend(preds.numpy())
+                total_loss+=loss.item()
+        ave_loss=total_loss/len(dataloader)
+        test_metrics=Metrics(all_labels,all_preds,self.config,self.id2label)
+        test_acc=test_metrics.acc
+        return ave_loss,test_acc
     
 def main(config_path="Bert_Config.json"):
     config=Cls_Config(config_path)
@@ -123,10 +158,11 @@ def main(config_path="Bert_Config.json"):
     
     train_dataset=TextClassificationDataset(config=config,dataset_type="train",label2id=label2id)
     dev_dataset=TextClassificationDataset(config=config,dataset_type="dev",label2id=label2id)
+    test_dataset=TextClassificationDataset(config=config,dataset_type="test",label2id=label2id)
 
     train_dataloader=DataLoader(train_dataset,config.batch_size,shuffle=True,collate_fn=train_dataset.collate_fn)
     dev_dataloader=DataLoader(dev_dataset,config.batch_size,shuffle=False,collate_fn=dev_dataset.collate_fn)
-   
+    test_dataloader=DataLoader(test_dataset,config.batchsize,shuffle=False,collate_fn=test_dataset.collate_fn)
     model=BertClassifier(config)
 
     optimizer=torch.optim.AdamW(
@@ -137,7 +173,7 @@ def main(config_path="Bert_Config.json"):
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=total_steps)
     
     trainer=Trainer(model,config,optimizer,scheduler,id2label)
-    trainer.train_with_early_stopping(train_dataloader,dev_dataloader)
+    trainer.save_checkpoint(train_dataloader,dev_dataloader,test_dataloader)
     
 if __name__=="__main__":
     config_path="config_exp/config_exp1.json"
